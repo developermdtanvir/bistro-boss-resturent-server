2b627ee49ffa64442a90e3bedac18b8a3e3cde1a
mdtanvirgit <freelencertanvirhossain@gmail.com>
2023-06-13 18:51:41 +0600

    remove token console debug

--- routes/jwtRoute.js
+++ routes/jwtRoute.js
@@ -5,7 +5,6 @@ const jwt = require('jsonwebtoken')
 router.post('/', (req, res) => {
     const email = req.body
     const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECREET, { expiresIn: '7d' });
-    console.log(token)
     res.send({ token });
 })
 
